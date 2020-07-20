aa23fb8ec0f1cf106f6b0873c45bc4e6fdfe040e
Emerson França <efrancabr@gmail.com>
2020-07-19 22:06:52 -0300

    Criação do catalogo de musicas

---
+++ src/main/java/com/fabio/catalogo/service/CatalogoService.java
@@ -0,0 +1,13 @@
+package com.fabio.catalogo.service;
+
+import java.util.List;
+
+import com.fabio.catalogo.model.Musica;
+
+public interface CatalogoService {
+    List<Musica> findAll();
+    Musica findById(long id);
+    Musica save(Musica musica);
+    void delete(long id);    
+    void update(Musica musica);   
+}
